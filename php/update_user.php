43d0f79abacf46550e3cf75168dcdce6df0f1346
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-06-25 21:31:32 -0300

    feito

--- php/update_user.php
+++ php/update_user.php
@@ -1,45 +1,49 @@
 <?php
 
-$id = $_GET ['codigo'];
-
+$id = $_GET['codigo'];
+ 
 include 'conexao.php';
-
+ 
 $select = "SELECT * FROM tb_user WHERE id_user = '$id'";
-
-
+ 
 $query = $conexao->query($select);
-
-$resultado = $query->fetch_assoc(); 
+ 
+$resultado = $query->fetch_assoc();
 ?>
-
-
-
+ 
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pr-BR">
+ 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Cadastro</title>
-  <link rel="stylesheet" href="../">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de Cadastro</title>
+    <link rel="stylesheet" href="../css/cadastro.css">
 </head>
+ 
 <body>
-    <form action="insert_user.php" method="post">
-    <div class="form-container">
-
-        <label for="nome">Nome</label>
-        <input type="text" name="nome" id="nome" value=""<?php echo $resultado ['nm_user'] 
-
-        <label for="email">Email</label>
-        <input type="text" name="email" id="email">
-
-        <label for="senha">Senha</label>
-        <input type="password" name="senha" id="senha">
-
-        <label for="setor">Setor</label>
-        <input type="number" name="numero" id="setor">
-
-        <a href="Home.html" class="botao">Cadastrar-se</a>
+    <div class="container">
+        <aside>
+            <img src="images/Img_Cadastro.png" alt="">
+        </aside>
+        <form action="#" method="post">
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" value="<?php echo $resultado['nm_user']; ?>" id="nome">
+ 
+            <label for="email">Email</label>
+            <input type="text" name="email" value="<?php echo $resultado['email']; ?>" id="email">
+ 
+            <label for="setor">Setor</label>
+            <input type="text" name="setor" value="<?php echo $resultado['setor']; ?>" id="setor">
+ 
+            <div class="botao">
+                <button type="submit">Editar</button>
+                <a href="">Voltar</a>
+            </div>
+        </form>
     </div>
-  </form>
+ 
 </body>
-</html> 
+ 
+</html>
+ 
